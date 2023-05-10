04c2cb36e7d28c616873fd1037a1aa1aa1d3765c
tungnk123 <doanthanhtungnk123@gmail.com>
2023-05-10 20:56:26 +0700

    Apply Mvvm structure for the project: change some file structure

--- app/src/main/java/com/example/penguin_project/view/fragment/TodoFragment.java
+++ app/src/main/java/com/example/penguin_project/view/fragment/TodoFragment.java
@@ -1,4 +1,4 @@
-package com.example.penguin_project.fragments;
+package com.example.penguin_project.view.fragment;
 
 import android.os.Bundle;
 
@@ -12,10 +12,10 @@ import com.example.penguin_project.R;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link StoreFragment#newInstance} factory method to
+ * Use the {@link TodoFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class StoreFragment extends Fragment {
+public class TodoFragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -26,7 +26,7 @@ public class StoreFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public StoreFragment() {
+    public TodoFragment() {
         // Required empty public constructor
     }
 
@@ -36,11 +36,11 @@ public class StoreFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment StoreFragment.
+     * @return A new instance of fragment TodoFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static StoreFragment newInstance(String param1, String param2) {
-        StoreFragment fragment = new StoreFragment();
+    public static TodoFragment newInstance(String param1, String param2) {
+        TodoFragment fragment = new TodoFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -61,6 +61,6 @@ public class StoreFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_store, container, false);
+        return inflater.inflate(R.layout.fragment_todo, container, false);
     }
 }
