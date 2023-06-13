4d3014150cf05fb9c23586c6aad386df7d8f1015
tungnk123 <doanthanhtungnk123@gmail.com>
2023-06-13 20:15:54 +0700

    Add SharedPreferences to store settings

--- app/src/main/java/com/example/penguin_project/SettingsChangeModeActivity.java
+++ app/src/main/java/com/example/penguin_project/SettingsChangeModeActivity.java
@@ -2,7 +2,9 @@ package com.example.penguin_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
@@ -12,12 +14,13 @@ import com.example.penguin_project.view.fragment.MenuFragment;
 public class SettingsChangeModeActivity extends AppCompatActivity {
 
     ImageButton btnBack;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings_change_mode);
         btnBack = findViewById(R.id.btn_activitySettingsCM_btnBack);
-
         btnBack.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -27,4 +30,5 @@ public class SettingsChangeModeActivity extends AppCompatActivity {
             }
         });
     }
+
 }
