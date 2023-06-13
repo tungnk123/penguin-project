dba3986898fb961e7dfc7da27b7e3c92219c2a80
tungnk123 <doanthanhtungnk123@gmail.com>
2023-06-13 19:07:21 +0700

    Add SettingsWeekStartAtActivity for settings

---
+++ app/src/main/java/com/example/penguin_project/SettingsWeekStartAt.java
@@ -0,0 +1,14 @@
+package com.example.penguin_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SettingsWeekStartAt extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings_week_start_at);
+    }
+}
