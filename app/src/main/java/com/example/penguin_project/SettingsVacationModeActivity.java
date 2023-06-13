9a57fcd1179b83ad5e78105b41031d2fb355794b
tungnk123 <doanthanhtungnk123@gmail.com>
2023-06-13 19:13:53 +0700

    Add SettingsVacationModeActivity for settings

---
+++ app/src/main/java/com/example/penguin_project/SettingsVacationModeActivity.java
@@ -0,0 +1,14 @@
+package com.example.penguin_project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SettingsVacationModeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings_vacation_mode);
+    }
+}
